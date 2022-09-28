f952d308f390e21a78232826a4260b2707f133fc
Rock <rock@simsage.co.uk>
2022-09-28 17:17:57 +0100

    updated copyright

--- sentence2vec_demo.py
+++ sentence2vec_demo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 #
-#  Copyright 2016-2018 Peter de Vocht
+#  Copyright 2016-2022 Peter de Vocht
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
